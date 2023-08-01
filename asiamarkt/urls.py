c9359fe80546670cb92d4d52e05b457f4974deb1
Aman <amanabiev10@gmail.com>
2023-08-01 23:37:41 +0200

    Initialer Commit

--- asiamarkt/urls.py
+++ asiamarkt/urls.py
@@ -7,4 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace="home")),
+    path('users', include('users.urls', namespace="users")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
